094d64cc41a742aad3d3f122f518f0b213f75c36
kirstenogleif <kirstenogleif@get2net.dk>
2017-04-22 06:25:04 +0200

    03

--- module-4/script.js
+++ module-4/script.js
@@ -36,7 +36,7 @@ WARNING!!! WARNING!!!
 
 (function(){ 
 
-var names = ["Yaakov1", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
